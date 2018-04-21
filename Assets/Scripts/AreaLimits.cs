9e3acfc42664573d48bc94f86087cc010c716d0d
Vladadamm <vladadamm@gmail.com>
2018-04-21 21:22:17 +0200

    Block Gen on fixed Columns

--- Assets/Scripts/AreaLimits.cs
+++ Assets/Scripts/AreaLimits.cs
@@ -20,5 +20,25 @@ public class AreaLimits {
 		return Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth*3/4,0,0)).x;
 	}
 
+	public static float CenterX(){
+		return (LeftLimit () + RightLimit ()) / 2;
+	}
+
+	public static float CenterY(){
+		return (UpLimit () + BottomLimit ()) / 2;
+	}
+
+	public static float SizeX(){
+		return (RightLimit () - LeftLimit ());
+	}
+
+	public static float SizeY(){
+		return (UpLimit () - BottomLimit ());
+	}
+
+	public static int BlockColumnsCount(){
+		return (int)Mathf.Floor (SizeX ());
+	}
+
 	//public const float areaWidth=0.5f;
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -23,18 +23,29 @@ public class LevelManager : MonoBehaviour {
 			t.position += Vector3.down*downSpeed * deltaTime;
 		}
 		if (spawnTimer < 0) {
-			float x=Random.Range(AreaLimits.LeftLimit()+0.5f,AreaLimits.RightLimit()-0.5f);
-			Vector3 pos = new Vector3(x, AreaLimits.UpLimit()+1+spawnTimer*downSpeed, 10);
+			int x=0;
 			spawnTimer += spawnInterval / downSpeed;
+			Vector3 pos = new Vector3(0, AreaLimits.UpLimit()+1+spawnTimer*downSpeed, 0);
 			float randBlock = Random.value;
-			GameObject g;
+			string chosenBlock;
 			if (randBlock < BlockWeightShort) {
-				g=(GameObject)GameObject.Instantiate (Resources.Load ("BlockShort"));
+				x=Random.Range(0,AreaLimits.BlockColumnsCount());
+				chosenBlock = "BlockShort";
+				lastSizeSpawned = 1;
 			} else if (randBlock < BlockWeightShort + BlockWeightMedium) {
-				g=(GameObject)GameObject.Instantiate (Resources.Load ("BlockMedium"));
+				x=Random.Range(0,AreaLimits.BlockColumnsCount()-1);
+				pos.x += 0.5f;
+				chosenBlock = "BlockMedium";
+				lastSizeSpawned = 2;
 			} else {
-				g=(GameObject)GameObject.Instantiate (Resources.Load ("BlockLong"));
+				x=Random.Range(1,AreaLimits.BlockColumnsCount()-1);
+				chosenBlock = "BlockLong";
+				lastSizeSpawned = 3;
 			}
+			lastColumnSpawned = x;
+			GameObject g;
+			pos.x += ConvertColumnToPos (x);
+			g=(GameObject)GameObject.Instantiate (Resources.Load (chosenBlock));
 			g.transform.position = pos;
 			g.transform.SetParent (transform);
 		}
@@ -47,9 +58,17 @@ public class LevelManager : MonoBehaviour {
 		}
 	}
 
+	float ConvertColumnToPos(int x){
+		return AreaLimits.CenterX() - AreaLimits.BlockColumnsCount () / 2f + x;
+	}
+
 	public float spawnInterval=1;
 	public float spawnTimer;
 	public float downSpeed=0.2f;
+
+
+	public int lastSizeSpawned=0;
+	public int lastColumnSpawned=0;
 
 	//Block Weights
 	public float BlockWeightShort = 0.3f;
