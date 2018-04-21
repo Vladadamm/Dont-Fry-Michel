b2d33a60721e30a36283c56ab5be0556155f5bac
Gcol <colletto.greogry@gmail.com>
2018-04-21 17:54:28 +0200

    First bullet in to the block

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public int vitesse = 200;
+	public	int life = 1;
+	Rigidbody2D	rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.velocity = new Vector2 (0, vitesse * Time.fixedDeltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.CompareTag ("Block")) {
+			collider.transform.GetComponent<Block> ().Damage (10);
+			life -= 1;
+			if (life <= 0)
+				Destroy (gameObject);
+		}
+	}
+}
