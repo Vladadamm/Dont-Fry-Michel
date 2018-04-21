e245b664a383afb90c7d9660ac5fc65402df55ea
Vladadamm <vladadamm@gmail.com>
2018-04-21 13:24:43 +0200

    Copie d'InputManager et autres scripts utilitaires

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InputManager {
+
+	private static InputManager instance;
+
+	public static InputManager getInstance()
+	{
+		if (instance == null) {
+			instance = new InputManager();
+		}
+		return instance;
+	}
+
+	public Dictionary<string, KeyCode> keyBinds{get; set;}
+
+	// Use this for initialization
+	private InputManager () {
+		keyBinds = new Dictionary<string, KeyCode> ();
+		keyBinds.Add ("Up", KeyCode.Z);
+		keyBinds.Add ("Down", KeyCode.S);
+		keyBinds.Add ("Left", KeyCode.Q);
+		keyBinds.Add ("Right", KeyCode.D);
+		keyBinds.Add ("Space", KeyCode.Space);
+		keyBinds.Add ("Esc", KeyCode.Escape);
+		keyBinds.Add ("LeftClick", KeyCode.Mouse0);
+		keyBinds.Add ("RightClick", KeyCode.Mouse1);
+	}
+	
+	//Redéfinition d'un keyBind 
+	public void bindKey(string key, KeyCode keyBind){
+		if (!keyBinds.ContainsValue(keyBind)) {
+			keyBinds [key] = keyBind;
+		}else{
+			string tmpK = keyBinds.FirstOrDefault(x => x.Value == keyBind).Key;
+			KeyCode tmpKB = keyBinds [key];
+			keyBinds [key] = keyBind;
+			keyBinds [tmpK] = tmpKB;
+		}
+	}
+}
---
+++ Assets/Scripts/SetKey.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetKey : MonoBehaviour {
+
+	string Key;
+
+	bool modifying;
+
+	// Use this for initialization
+	void Start () {
+		Key = this.gameObject.transform.GetChild (1).GetComponent<Text>().text;
+		modifying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (modifying == true) {
+			KeyCode newKey = KeyCode.None;
+			foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode))) {
+				if (Input.GetKeyDown (kcode)) {
+					newKey = kcode;
+				}
+			}
+			if (newKey != KeyCode.None) {
+				InputManager.getInstance ().bindKey(Key, newKey);
+				modifying = false;
+			}
+		}
+		this.gameObject.transform.GetChild (0).GetComponent<Text> ().text = InputManager.getInstance ().keyBinds [Key].ToString ();
+	}
+
+	// Rebind on click
+	public void Rebind(){
+		modifying = true;
+	}
+}
