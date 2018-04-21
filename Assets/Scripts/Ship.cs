925fda3c9ed89968d755d433699160e7dd182d5f
Gcol <colletto.greogry@gmail.com>
2018-04-21 17:58:17 +0200

    ship v2

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour {
 
+	public float fire_rate = 0.5f;
+	float last_shot;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,8 +18,11 @@ public class Ship : MonoBehaviour {
 			transform.position = new Vector2 (transform.position.x + 3f * Time.fixedDeltaTime, transform.position.y);
 		} else if (Input.GetKey (InputManager.getInstance ().keyBinds ["Ship_left"])) {
 			transform.position = new Vector2 (transform.position.x - 3f * Time.fixedDeltaTime, transform.position.y);
-		} else if (Input.GetKey (InputManager.getInstance ().keyBinds ["Ship_fire"])) {
-			Debug.Log ("Fire");
+		} 
+		if (Input.GetKey (InputManager.getInstance ().keyBinds ["Ship_fire"]) && (Time.fixedTime > last_shot + fire_rate)) {
+			GameObject bullet = (GameObject)GameObject.Instantiate (Resources.Load ("Bullet"));
+			bullet.transform.position = transform.position;
+			last_shot = Time.fixedTime;
 		}
 	}
 }
