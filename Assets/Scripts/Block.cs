1a1edc91766aa6b1bf2608533b4e21227e9d3208
Vladadamm <vladadamm@gmail.com>
2018-04-21 17:21:38 +0200

    LevelManager + Block

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Damage(int dmg){
+		health -= dmg;
+		if (health <= 0) {
+			GameObject.Destroy (gameObject);
+		} else {
+			transform.GetComponent<SpriteRenderer>().color = Color.white * (health / (float)maxHealth);
+		}
+	}
+
+	public int maxHealth;
+	public int health;
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		spawnTimer = spawnInterval / downSpeed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		for (int i = 0; i < transform.childCount; ++i) {
+			Transform t = transform.GetChild (i);
+			if (Camera.main.WorldToScreenPoint (t.position + Vector3.down).y < 0)
+				t.GetComponent<Block> ().Damage (1);
+			t.position += Vector3.down*downSpeed * Time.fixedDeltaTime;
+		}
+		if (spawnTimer < 0) {
+			int x=Random.Range(0,Camera.main.pixelWidth);
+			Vector3 pos = Camera.main.ScreenToWorldPoint (new Vector3(x, Camera.main.pixelHeight, 10)) + Vector3.up * spawnTimer/downSpeed;
+			spawnTimer += spawnInterval / downSpeed;
+			GameObject g = (GameObject)GameObject.Instantiate (Resources.Load ("Block"));
+			g.transform.position = pos;
+			g.transform.SetParent (transform);
+		}
+		spawnTimer -= Time.fixedDeltaTime;
+	}
+
+	public float spawnInterval=1;
+	public float spawnTimer;
+	public float downSpeed=0.2f;
+}
