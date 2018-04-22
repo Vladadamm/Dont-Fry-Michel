f142afc56f590de924f0ffd7a6cd3b3b38cba2bd
Gcol <colletto.greogry@gmail.com>
2018-04-22 11:13:14 +0200

    Debut de changement pipette

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ship : MonoBehaviour {
-	public Animation	animation;
+
+	private SpriteRenderer spriteRenderer; 
+
+	public	int		nb_sprite = 10;
 	public	float	size_sprite = 0.2f;
 	public	float	acceleration = 5f;
 	public float	max_speed = 5f;
@@ -12,29 +15,26 @@ public class Ship : MonoBehaviour {
 	float	speed;
 	float last_load;
 	int nb_bullet;
+	Object [] sprites;
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3 ((AreaLimits.LeftLimit () + AreaLimits.RightLimit ()) / 2, AreaLimits.BottomLimit ()+0.5f, 0);
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		transform.position = new Vector3 ((AreaLimits.LeftLimit () + AreaLimits.RightLimit ()) / 2, AreaLimits.BottomLimit () + 1f, 0);
 		enabled = false;
 		nb_bullet = max_bullet;
+		sprites = Resources.LoadAll ("pipette-sprites-01");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		InputM();
-		//AnimationM ();
 		if (transform.position.x + speed * Time.fixedDeltaTime > AreaLimits.LeftLimit() + size_sprite && transform.position.x + speed * Time.fixedDeltaTime < AreaLimits.RightLimit() - size_sprite)
 			transform.position = new Vector3 (transform.position.x + speed * Time.fixedDeltaTime, transform.position.y);
 		else
 			speed = -(speed / 2);
 
 	}
-
-	/*void AnimationM(){
-		if (Time.fixedTime < last_shot + fire_rate)
-			animation.Play();
-	}*/
 
 	void InputM(){
 		if (Input.GetKey (InputManager.getInstance ().keyBinds ["Ship_right"])) {
@@ -54,12 +54,14 @@ public class Ship : MonoBehaviour {
 				nb_bullet--;
 				GameObject bullet = (GameObject)GameObject.Instantiate (Resources.Load ("Bullet"));
 				bullet.transform.position = transform.position;
-				
 			}
 		}else if (Time.fixedTime >= last_load + load_rate && nb_bullet < max_bullet) {
 			nb_bullet += 1;
 			last_load = Time.fixedTime;
 		}
+		spriteRenderer.sprite = (Sprite)sprites [nb_bullet / max_bullet * ];
+		Debug.Log (0 + nb_bullet);
+
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
